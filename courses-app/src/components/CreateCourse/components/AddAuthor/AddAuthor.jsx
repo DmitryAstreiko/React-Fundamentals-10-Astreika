67022d57496a7957bfc91a9425a6f48fde0b015b
D.Astreika <dzmitry_astreika@epam.com>
2022-06-28 01:54:42 +0300

    Adding author - in progress

--- courses-app/src/components/CreateCourse/components/AddAuthor/AddAuthor.jsx
+++ courses-app/src/components/CreateCourse/components/AddAuthor/AddAuthor.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../../../../App.css';
 import Button from '../../../../common/Button/Button';
 import Input from '../../../../common/Input/Inpit';
+import { v4 as uuid } from 'uuid';
 
 function AddAuthor(props) {
 	const [nameAuthor, setnameAuthor] = useState(null);
@@ -9,7 +10,11 @@ function AddAuthor(props) {
 		<div className='AddAuthorMain'>
 			<label className='CreateCourseLabelsParagraph'>Add author</label>
 			<label className='CreateCourseLabels'>Author name</label>
-			<Input placeholderText='Enter author name...' onChange={onInputText} />
+			<Input
+				placeholderText='Enter author name...'
+				onChange={onInputText}
+				needClear='true'
+			/>
 			<div style={{ textAlign: 'center' }}>
 				<Button buttonText='Create author' onButtonPress={addAuthor} />
 			</div>
@@ -17,10 +22,11 @@ function AddAuthor(props) {
 	);
 
 	function addAuthor() {
-		setnameAuthor((current) => [
-			...current,
-			{ id: new Guid(), name: { nameAuthor } },
-		]);
+		console.log('nameAuthor + ' + nameAuthor);
+
+		props.itemsAuthors.push({ id: uuid(), name: { nameAuthor } });
+
+		console.log(props.itemsAuthors);
 	}
 
 	function onInputText(name) {
